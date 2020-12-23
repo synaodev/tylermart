27a8bd2c9449b6209c584dd92b7305a769794429
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-23 14:16:20 -0800

    creates skeleton sln

---
+++ TylerMart.Local/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TylerMart.Local {
+	internal class Program {
+		private static void Main(string[] args) {
+			Console.WriteLine("Hello, world!");
+		}
+	}
+}
---
+++ TylerMart.Testing/UnitTest.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace TylerMart.Testing {
+	public class UnitTest {
+		[Fact]
+		public void Test() {
+
+		}
+	}
+}
