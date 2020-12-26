261efce3e31ce520ce7f3fa7e857a34f744effbf
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-12-26 14:45:57 -0800

    added more tests

--- TylerMart.Testing/RepositoryTests.cs
+++ TylerMart.Testing/RepositoryTests.cs
@@ -13,7 +13,7 @@ namespace TylerMart.Testing {
 		/// Checks for seeded data in database
 		/// </summary>
 		[Fact]
-		public void TestSeededDatabase() {
+		public void TestSeededData() {
 			DatabaseService db = new DatabaseService();
 
 			List<Customer> customers = db.Customers.All();
@@ -27,6 +27,21 @@ namespace TylerMart.Testing {
 
 			List<Order> orders = db.Orders.All();
 			Assert.Equal(2, orders.Count);
+		}
+		/// <summary>
+		/// Checks for valid many-to-many relationships in database
+		/// </summary>
+		[Fact]
+		public void TestSeededManyToManyData() {
+			DatabaseService db = new DatabaseService();
+
+			Location location = db.Locations.Get(1);
+			List<Product> productsAtLocation = db.Products.FindFromLocation(location);
+			Assert.Equal(4, productsAtLocation.Count);
+
+			Order order = db.Orders.Get(1);
+			List<Product> productsInOrder = db.Products.FindFromOrder(order);
+			Assert.Equal(2, productsInOrder.Count);
 		}
 	}
 }
