c45e991a5390a24dad07e49a2a7b51b12234804f
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-01-08 10:41:38 -0800

    endpoints

--- TylerMart.IntnTest/BasicTests.cs
+++ TylerMart.IntnTest/BasicTests.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Net.Http;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 using TylerMart.Client;
@@ -8,17 +5,21 @@ using TylerMart.IntnTest.Utility;
 
 namespace TylerMart.IntnTest {
 	public class BasicTests : IClassFixture<MartFactory<Startup>> {
-		private readonly HttpClient Client;
 		private readonly MartFactory<Startup> Factory;
 		public BasicTests(MartFactory<Startup> factory) {
 			Factory = factory;
-			Client = factory.CreateClient(new WebApplicationFactoryClientOptions() {
-				AllowAutoRedirect = false
-			});
 		}
-		[Fact]
-		public async void TestInitialization() {
-			var defaultPage = await Client.GetAsync("/");
+		[Theory]
+		[InlineData("/")]
+		[InlineData("/Home/Index")]
+		[InlineData("/Home/Privacy")]
+		[InlineData("/Customer/Login")]
+		[InlineData("/Customer/Register")]
+		public async void TestEndpoints(string url) {
+			var client = Factory.CreateClient();
+			var response = await client.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+			Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
 		}
 	}
 }
