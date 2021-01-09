9fed7ccb1946acac6ec97d7bbf3201ac6967bad8
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-01-08 21:16:35 -0800

    trying to figure out csrf

--- TylerMart.IntnTest/BasicTests.cs
+++ TylerMart.IntnTest/BasicTests.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Encodings;
 using Xunit;
 
 using TylerMart.Client;
+using TylerMart.Client.Models;
 using TylerMart.IntnTest.Utility;
 
 namespace TylerMart.IntnTest {
@@ -18,8 +25,46 @@ namespace TylerMart.IntnTest {
 		public async void TestEndpoints(string url) {
 			var client = Factory.CreateClient();
 			var response = await client.GetAsync(url);
-			response.EnsureSuccessStatusCode();
+			Assert.True(response.IsSuccessStatusCode);
 			Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+		}
+		[Fact]
+		public async void TestRegisterAndLogin() {
+			var client = Factory.CreateClient();
+			// client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+			// 	"Basic",
+			// 	Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", )))
+			// );
+
+			var registerGet = await client.GetAsync("/Customer/Register");
+
+			var registerForm = new List<KeyValuePair<string, string>>() {
+				new KeyValuePair<string, string>("FirstName", "Tyler"),
+				new KeyValuePair<string, string>("LastName", "Cadena"),
+				new KeyValuePair<string, string>("Email", "tyler.cadena@revature.net"),
+				new KeyValuePair<string, string>("Password", "tylercadena"),
+				new KeyValuePair<string, string>("PasswordConfirmation", "tylercadena"),
+				new KeyValuePair<string, string>("Address", "23222 Remington Way, West Hills, CA, 91307")
+			};
+			var registerResponse = await client.PostAsync(
+				"/Customer/Register",
+				new FormUrlEncodedContent(registerForm)
+			);
+			Assert.True(registerResponse.IsSuccessStatusCode);
+			var loginForm = new List<KeyValuePair<string, string>>() {
+				new KeyValuePair<string, string>("Email", "tyler.cadena@revature.net"),
+				new KeyValuePair<string, string>("Password", "tylercadena")
+			};
+			var loginResponse = await client.PostAsync(
+				"/Customer/Login",
+				new FormUrlEncodedContent(loginForm)
+			);
+			Assert.True(loginResponse.IsSuccessStatusCode);
+			Assert.Equal("/Customer/Index", loginResponse.RequestMessage.RequestUri.AbsolutePath);
 		}
 	}
 }
+
+
+
+//Assert.Equal("/Customer/Index", response3.RequestMessage.RequestUri.AbsolutePath);
